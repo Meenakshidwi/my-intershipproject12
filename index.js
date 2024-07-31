4884e68008c70ffe7bf5747728343d34219e379b
Meenakshidwi <155650789+Meenakshidwi@users.noreply.github.com>
2024-07-31 19:42:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/** @format */
+
+// JavaScript to load common header and footer
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
